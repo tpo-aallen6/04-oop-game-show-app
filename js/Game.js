68895cb08492c8164f76d85174ecc62ba034f785
tpo-aallen6 <77011857+tpo-aallen6@users.noreply.github.com>
2021-02-05 12:25:14 -0500

    comments
    added comments

--- js/Game.js
+++ js/Game.js
@@ -13,7 +13,7 @@ class Game {
       new Phrase('and the gunslinger followed'),
       new Phrase('That was falling with style'),
       new Phrase('Reach for the sky'),
-      new Phrase('Cool cool cool cool no doubt'),
+      new Phrase('May the force be with you'),
       new Phrase('For the Horde'),
       new Phrase('Khajiit has wares if you have coin'),
       new Phrase('There is no Pepe Silvia')
@@ -45,10 +45,20 @@ class Game {
     this.activePhrase.addPhraseToDisplay()
   }
 
+  /**
+  * Selects random phrase from phrases property
+  * @return {Object} random Phrase object chosen to be used
+  **/
+
   getRandomPhrase () {
     const random = Math.floor(Math.random() * this.phrases.length)
     return this.phrases[random]
   }
+
+  /**
+  * Handles onscreen keyboard button clicks
+  * @param (HTMLButtonElement) button - The clicked button element
+  **/
 
   handleInteraction (button) {
     const isLetterInPhrase = this.activePhrase.checkLetter(button.textContent)
@@ -68,6 +78,12 @@ class Game {
     }
   }
 
+  /**
+  * Increases the value of the missed property
+  * Removes a life from the scoreboard
+  * Checks if player has remaining lives and ends game if player is out
+  **/
+
   removeLife () {
     const heartImages = document.querySelectorAll('li.tries img')
     heartImages[this.missed].src = 'images/lostHeart.png'
@@ -78,11 +94,21 @@ class Game {
     }
   }
 
+  /**
+  * Checks for winning move
+  * @return {boolean} True if game has been won, false if game wasn't won
+  **/
+
   checkForWin () {
     return Array
       .from(document.querySelectorAll('#phrase ul li.letter'))
       .every(element => element.classList.contains('show'))
   }
+
+  /**
+  * Displays game over message
+  * @param (string) result - 'win' for a win, 'lose' for a loss
+  **/
 
   gameOver (result) {
     document.querySelector('#overlay').style.display = 'block'
--- js/Phrase.js
+++ js/Phrase.js
@@ -8,6 +8,10 @@ class Phrase {
     this.phrase = phrase.toLowerCase()
   }
 
+  /**
+  * Display phrase on game board
+  **/
+
   addPhraseToDisplay () {
     const ul = document.querySelector('#phrase ul')
     for (let i = 0; i < this.phrase.length; i++) {
@@ -25,9 +29,19 @@ class Phrase {
     }
   }
 
+  /**
+  * Checks if passed letter is in phrase
+  * @param (string) letter - Letter to check
+  **/
+
   checkLetter (letter) {
     return this.phrase.includes(letter.toLowerCase())
   }
+
+  /**
+  * Displays passed letter on screen after a match is found
+  * @param (string) letter - Letter to display
+  **/
 
   showMatchedLetter (letter) {
     letter = letter.toLowerCase()
